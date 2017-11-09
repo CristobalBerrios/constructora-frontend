a7c8fd5a91edf7e0500fcd2b50f84512c2661ee3
Cristobal Berrios <elcriss96@gmail.com>
2017-11-09 19:20:08 -0300

    modificado produccion

--- src/constants/address.js
+++ src/constants/address.js
@@ -1,8 +1,5 @@
 export const address = {
   base: 'https://api-constructora.herokuapp.com/',
-  /*
-  api: 'http://localhost:8000/api/',
-  */
   api: 'https://api-constructora.herokuapp.com/api/',
   spa: 'http://localhost:8080'
 }
