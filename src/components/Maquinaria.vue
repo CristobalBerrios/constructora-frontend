411942db046bb77e960a5497bb28897e822357b8
Cristobal Berrios <elcriss96@gmail.com>
2017-11-09 02:29:38 -0300

    modificado formulario para agregar, se agrego lista de marcas y ubicaciones

--- src/components/Maquinaria.vue
+++ src/components/Maquinaria.vue
@@ -15,61 +15,117 @@
       <td>{{ props.item.modelo.descripcion }}</td>
     </template>
   </v-data-table>
-  <v-btn fab dark color="indigo" class="btn-flotante" @click.stop="dialog = true">
+  <v-btn fab dark color="blue-grey darken-3" class="btn-flotante" @click="showDialog()">
     <v-icon dark>add</v-icon>
   </v-btn>
 
+  <br> <br>
+
+  <v-layout wrap justify-space-between>
+    <v-flex xs12 md3>
+      <v-card dark>
+        <v-toolbar flat dark color="blue-grey darken-3">
+          <v-toolbar-title class="white--text">Marcas</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon @click.native="dialog = false">
+            <v-icon>add</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-list class="grey lighten-1">
+          <template>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title> Kobelco</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                 <v-btn dark icon @click.native="dialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title> Kobelco</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn flat >Eliminar</v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title> Kobelco</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn flat >Eliminar</v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title> Kobelco</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn flat >Eliminar</v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </template>
+        </v-list>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 md3>
+      <v-card>
+    <v-toolbar flat dark color="blue-grey darken-3">
+      <v-toolbar-title class="white--text">Ubicaciones</v-toolbar-title>
+    </v-toolbar>
+  </v-card>
+    </v-flex>
+  </v-layout>
+
   <v-dialog v-model="dialog" persistent max-width="500px">
       <v-card>
-        <v-card-title>
-          <span class="headline">User Profile</span>
-        </v-card-title>
+        <v-toolbar flat dark color="blue-grey darken-3">
+          <v-toolbar-title class="white--text">Nueva Maquinaria</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon @click.native="dialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-toolbar>
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal first name" required></v-text-field>
+              <v-flex md5>
+                <v-text-field label="Patente" required></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+              <v-flex md5>
+                <v-text-field label="Identificacion" hint="example of helper text only on focus"></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal last name" hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
+              <v-flex md2>
+                <v-text-field label="Año" required></v-text-field>
               </v-flex>
               <v-flex xs12>
-                <v-text-field label="Email" required></v-text-field>
+                <v-select
+                  label="Marca"
+                  :items="marcas"
+                  item-text="descripcion"
+                  item-value="id"
+                  required
+                ></v-select>
               </v-flex>
               <v-flex xs12>
-                <v-text-field label="Password" type="password" required></v-text-field>
+            <v-text-field
+              label="Descripcion"
+              multi-line
+              rows="2"
+            ></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6>
-                <v-select
-                  label="Age"
-                  required
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                ></v-select>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-select
-                  label="Interests"
-                  multiple
-                  autocomplete
-                  chips
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                ></v-select>
+              <v-flex xs12>
+                <v-btn block dark color="blue-grey darken-3" raised @click.native="dialog = false">Agregar</v-btn>
               </v-flex>
             </v-layout>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
-        </v-card-actions>
       </v-card>
     </v-dialog>
   </section>
@@ -77,6 +133,7 @@
 
 <script>
 import { maquinariaService } from '@/services/Maquinaria.service'
+import { marcaService } from '@/services/Marca.service'
 
 export default {
   data () {
@@ -90,7 +147,8 @@ export default {
         {text: 'Modelo', value: 'modelo', sortable: false, align: 'left'}
       ],
       items: [],
-      dialog: false
+      dialog: false,
+      marcas: []
     }
   },
   mounted () {
@@ -98,6 +156,16 @@ export default {
       console.log(data.body)
       this.items = data.body
     })
+  },
+  methods: {
+    showDialog () {
+      let vm = this
+      vm.dialog = true
+      marcaService.getAll().then(data => {
+        vm.marcas = data.body
+        console.log(vm.marcas)
+      })
+    }
   }
 }
 </script>
