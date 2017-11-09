4a9fc3731553e06159f03ed3672eeeb14a11a7c5
Cristobal Berrios <elcriss96@gmail.com>
2017-11-09 19:48:40 -0300

    arreglado las urls

--- src/constants/address.js
+++ src/constants/address.js
@@ -1,8 +1,5 @@
 export const address = {
   base: 'http://localhost:8000',
   api: 'https://api-constructora.herokuapp.com/api/',
-  /*
-  api: 'http://10.0.2.99:8888/Startup/Constructora/public/api/',
-  */
   spa: 'http://localhost:8080'
 }
