6249d58a424443e3f643f1b430abb5e9b334347f
Cristobal Berrios <elcriss96@gmail.com>
2017-11-15 09:19:08 -0300

    fix de el servicio

--- src/services/Marca.service.js
+++ src/services/Marca.service.js
@@ -1,25 +1,9 @@
 import ApiService from './Api.service.js'
-import Vue from 'vue'
-import VueResource from 'vue-resource'
-
-Vue.use(VueResource)
 
 class MarcaService extends ApiService {
   constructor (endpoint) {
     super(endpoint)
     this.name = endpoint
-  }
-
-  getAll () {
-    return this.getResource().query()
-  }
-
-  getById (id) {
-    return this.getResource().get({id: id})
-  }
-
-  save (id, model) {
-    return this.getResource().save({id: id}, {marca: model})
   }
 }
 
