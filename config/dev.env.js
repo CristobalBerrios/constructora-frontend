4c7df0f2710154ccb7662fe782ced5ef33100437
Cristobal Berrios <elcriss96@gmail.com>
2017-11-30 09:44:41 -0300

    agregadas rutas de api de bot

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,9 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   address: {
     base: '"http://localhost:8000/"',
-    api: '"http://localhost:8000/api/"',
-    spa: '"http://localhost:8080"'
+    api: '"http://localhost:8000/api/v1/"',
+    spa: '"http://localhost:8080"',
+    apiBot: '"https://api.dialogflow.com/v1/"',
+    tokenBot: '"Bearer f6ba9d5679854013a5b5f2abf989a4e4"'
   },
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,9 @@ module.exports = {
   NODE_ENV: '"production"',
   address: {
     base: '"https://api-constructora.herokuapp.com/"',
-    api: '"https://api-constructora.herokuapp.com/api/"',
-    spa: '"http://localhost:8080"'
+    api: '"https://api-constructora.herokuapp.com/api/v1"',
+    spa: '"http://localhost:8080"',
+    apiBot: '"https://api.dialogflow.com/v1/"',
+    tokenBot: '"Bearer f6ba9d5679854013a5b5f2abf989a4e4"'
   },
 }
